4c797a48905bb45ad610dedf3a48ab901b549b88
Bhuv <bhuv059@gmail.com>
2026-02-26 17:49:14 +0100

    coderabbit suggestions

--- app/routes/visualizer.$id.tsx
+++ app/routes/visualizer.$id.tsx
@@ -1,7 +1,7 @@
 import {useLocation, useNavigate} from "react-router";
 import {useEffect, useRef, useState} from "react";
 import {generate3DView} from "../../lib/ai.action";
-import {Box, Download, RefreshCcw, Share, Share2, X} from "lucide-react";
+import {Box, Download, RefreshCcw, Share2, X} from "lucide-react";
 import Button from "../../components/ui/Button";
 
 const VisualizerId = () => {
@@ -62,7 +62,7 @@ const VisualizerId = () => {
 				<nav className="topbar">
 					<div className="brand">
 						<Box className="logo" />
-						<span className="name">Roomify</span>
+						<span className="name">HomeArchi</span>
 					</div>
 					<Button variant="ghost" size="sm" onClick={handleBack} className="exit">
 						 <X className="icon" />Exit Editor
@@ -88,7 +88,7 @@ const VisualizerId = () => {
 
 						<div className={`render-area ${isProcessing? 'is-processing' : ''}`} >
 							{currentImage? (
-								<img src={currentImage} alt="AI Render" className="render.img" />
+								<img src={currentImage} alt="AI Render" className="render-img" />
 							):(
 								<div className="render-placeholder">
 									{initialImage && (
